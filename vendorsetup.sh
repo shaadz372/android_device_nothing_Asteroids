a93e8d1da37efb5466188e9d73e13de0888b20da
shaadz372 <shaadz3722@gmail.com>
2025-05-17 12:31:05 +0530

    Update vendorsetup.sh
    Revert

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# add_lunch_combo 
-twrp_Asteroids-user
-twrp_Asteroids-userdebug
-twrp_Asteroids-eng
+add_lunch_combo twrp_Asteroids-user
+add_lunch_combo twrp_Asteroids-userdebug
+add_lunch_combo twrp_Asteroids-eng
