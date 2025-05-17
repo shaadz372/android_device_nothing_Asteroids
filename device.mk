f3a8804f4676a98e6ab057c567844fb6c98843ed
shaadz372 <shaadz3722@gmail.com>
2025-05-17 11:24:41 +0530

    Update device.mk
    Fix Build error

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.volcano
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+PRODUCT_STATIC_BOOT_CONTROL_HAL += \
     bootctrl.volcano \
     libgptutils \
     libz \
